ab64a8d491cb1057b1a249f93b6ddc836cf1de47
MishaR <mesharoman12@gmail.com>
2021-11-15 12:40:14 +0200

    Регистрация

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,10 @@
 <?php
-	// Инициируем константы
-	define('SITE_NAME', 'Twitter');
-	define('HOST', 'http://' . $_SERVER['HTTP_HOST']);
-	define('DB_HOST', 'localhost');
-	define('DB_NAME', 'twitter');
-	define('DB_USER', 'root');
-	define('DB_PASS', 'root');
+// Инициируем константы
+define('SITE_NAME', 'Twitter');
+define('HOST', 'http://' . $_SERVER['HTTP_HOST']);
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'twitter');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+
+session_start();
--- user_posts.php
+++ user_posts.php
@@ -7,6 +7,8 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
 }
 $posts = get_posts($id);
 
+$title = 'Твиты пользователя @' . $posts[0]['login'];
+
 include_once "includes/header.php";
 include_once "includes/posts.php";
 include_once "includes/footer.php";
